bcf633c2fce39ddc042e4ef1aa74a47774319a84
Ajay <persauaj@sheridancollege.ca>
2023-01-28 15:55:26 -0500

    movement and prompt if the wrong role is inputted

--- Python Assignment 1/Game.py
+++ Python Assignment 1/Game.py
@@ -6,21 +6,33 @@ def start_game():
     print("WELCOME TO THE TREASURE HUNT")
     print("YOUR GOAL IS TO FIND THE ONE PIECE")
 
-#Chose your role
-print("Choose your role: (priate/warrior)")
-role = input().strip().capitalize()
+#Choose your role
+while True:
+    print("Choose your role: (Priate/Warrior)")
+    role = input().strip().capitalize()
+    if role == "priate":
+        player = Priate()
+        break
+    elif role == "warrior":
+        player = Warrior()
+        break
+    else:
+        print("Invalid role choice.(Priate/Warrior)")
 
 while True:
     print("Which direction would you like to go? (East, West, North, South)")
-    direction = input()
+    direction = input().strip().capitalize()
+
 
     #Movement
     if direction == "East":
-        print("You have moved towards the east")
+        print("You have moved towards the East")
     elif direction == "South":
          print("You have moved towards the South")
     elif direction == "West":
          print("You have moved towards the West")
+    elif direction == "North":
+         print("You have moved towards the North")
     else:
         print("Invalid Input You have moved towards the (East, West, North, South")
 
