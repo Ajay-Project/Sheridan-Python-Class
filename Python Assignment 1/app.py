d521c4b21ad4fa1e4ae963555997e12b72cba29a
Ajay <persauaj@sheridancollege.ca>
2023-01-29 19:48:25 -0500

    Intro & present role

--- Python Assignment 1/app.py
+++ Python Assignment 1/app.py
@@ -1,1 +1,8 @@
 #implements the interactivity with the user
+
+def start_game():
+    print("WELCOME TO THE HUNT FOR THE ONE PIECE")
+    print("THE ONE PIECE TREASURE IS THE GIFT OF THE UNKNOW ")
+    print("1. Pirate")
+    print("2. Warrior")
+    role = input()
