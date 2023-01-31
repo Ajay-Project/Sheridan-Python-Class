e9dfe2ef1238d7b45bf3e2d4c0383691db0a6332
Ajay <persauaj@sheridancollege.ca>
2023-01-31 14:35:41 -0500

    try

--- class 7/loop.py
+++ class 7/loop.py
@@ -8,11 +8,23 @@
 #print(sumTo(6))
 
 
-def factorial(number):
-    theNumber=1
-    for number in range(1,number +1):
-        theNumber = theNumber * number
-    return theNumber
-    
-number = int(input("ENTER A NUMBER: "))
-print(factorial(number))
+#def factorial(number):
+#    theNumber=1
+#    for number in range(1,number +1):
+#        theNumber = theNumber * number
+#    return theNumber
+#    
+#number = int(input("ENTER A NUMBER: "))
+#print(factorial(number))
+
+
+#for o in range(1,4):
+#    for n in range(1,5):
+#        print(n, end="")
+#    print()
+
+
+for r in range(1,5):
+    for c in range(1, r+1):
+        print(c, end="")
+    print()
