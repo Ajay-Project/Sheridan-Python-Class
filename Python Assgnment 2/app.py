6b3ca6b9ba8ad1c56cfa31176832ec7f6c7cbd9d
Ajay <persauaj@sheridancollege.ca>
2023-03-16 09:30:50 -0400

    created stock prediction function

--- Python Assgnment 2/app.py
+++ Python Assgnment 2/app.py
@@ -1,9 +1,2 @@
 #Display Info
 import product
-
-
-
-myproduct = product.Product(1 , "A", 54, 2, 12, 43)
-myproduct.get_product_info() 
-
-myproduct.simulate_monthly()
--- Python Assgnment 2/product.py
+++ Python Assgnment 2/product.py
@@ -23,17 +23,22 @@ class Product:
         return f"Product Code: {self.productCode}\nProduct Name: {self.productName}\nSale Price: ${self.productSalePrice}\nManufacture Cost: ${self.prodManuCost}\nMonthly Production: {self.UnitManu}\n"
 
 
-    def simulate_monthly(self,predictStock):
+    def simulate_monthly(self):
         #SALE OF PRODUCT
-        unitSold = random.randint(self.UnitManu - 10 or self.UnitManu + 10)
+        unitSold = random.randint(self.UnitManu - 10,self.UnitManu + 10)
         sale_of_product = unitSold
-        predictStock = self.UnitManu - self.unitSold
+        predictStock = self.UnitManu - sale_of_product
         return (sale_of_product,predictStock)
 
     
     def stock_prediction(self):
         for i in range(1,13):
+            maunfactured_unit = self.UnitManu + random.randint(0,200)
+            unit_sold = random.randint(0, maunfactured_unit)
+            stock_remain = maunfactured_unit - unit_sold
+            return (maunfactured_unit,unit_sold,stock_remain)
 
+            
 
 
 
